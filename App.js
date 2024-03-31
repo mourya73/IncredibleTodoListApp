1792839717ff37172b4f9e6d7b9cffbae1d19975
Mourya Shah <mshah3173@gmail.com>
2024-03-30 23:37:01 -0600

    Add ToDoForm and ToDoList components

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-// GitHub:https://github.com/mourya73/IncredibleTodoListApp
-
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import ToDoForm from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ToDoForm />
+      <ToDoList />
+      
+    </SafeAreaView>
   );
 }
 
